49234c1059b4ae4e7b91b44f6f390b6aded21844
joshschcom <84538564+joshschcom@users.noreply.github.com>
2023-07-30 14:05:45 +0200

    Create deployERC20.js

---
+++ scripts/deployERC20.js
@@ -0,0 +1,19 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  const TestToken = await ethers.getContractFactory("TestToken");
+  const testToken = await TestToken.deploy();
+
+  console.log("TestToken address:", testToken.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
